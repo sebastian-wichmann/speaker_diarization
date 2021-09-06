7d4599058ec3c67ebf950730e844127dbc51b02e
Sebastian Wichmann <Sebastian.Wichmann1998@web.de>
2021-09-06 08:10:26 +0200

    Remove unused parameter

--- src/pynn/bin/decode_x_vector.py
+++ src/pynn/bin/decode_x_vector.py
@@ -22,14 +22,10 @@ parser.add_argument('--downsample', help='concated frames', type=int, default=1)
 parser.add_argument('--mean-sub', help='mean subtraction', action='store_true')
 
 parser.add_argument('--fp16', help='float 16 bits', action='store_true')
-parser.add_argument('--state-cache', help='caching encoder states', action='store_true')
 parser.add_argument('--batch-size', help='batch size', type=int, default=32)
 parser.add_argument('--beam-size', help='beam size', type=int, default=10)
-parser.add_argument('--max-len', help='max len', type=int, default=100)
-parser.add_argument('--coverage', help='coverage term', type=float, default=0.0)
-parser.add_argument('--len-norm', help='length normalization', action='store_true')
-parser.add_argument('--output', help='output file', type=str, default='hypos/H_1_LV.ctm')
-parser.add_argument('--format', help='output format', type=str, default='ctm')
+parser.add_argument('--output', help='output file', type=str, default='hypos/H_1_LV')
+parser.add_argument('--format', help='output format', type=str, default='txt')
 parser.add_argument('--space', help='space token', type=str, default='▁')
 
 if __name__ == '__main__':
@@ -44,21 +40,11 @@ if __name__ == '__main__':
     model.load_state_dict(mdic['state'])
     model.eval()
     if args.fp16: model.half()
- 
-    lm = None
-    if args.lm_dic is not None:
-        mdic = torch.load(args.lm_dic)
-        lm = load_object(mdic['class'], mdic['module'], mdic['params'])
-        lm = lm.to(device)
-        lm.load_state_dict(mdic['state'])
-        lm.eval()
-        if args.fp16: lm.half()
-        
+    
     reader = SpectroDataset(args.data_scp, mean_sub=args.mean_sub, fp16=args.fp16,
                             downsample=args.downsample)
     since = time.time()
     fout = open(f"{args.output}.{args.format}", 'w')
-    decode_func = beam_search_cache if args.state_cache else beam_search
     with torch.no_grad():
         while True:
             seq, mask, utts = reader.read_batch_utt(args.batch_size)
