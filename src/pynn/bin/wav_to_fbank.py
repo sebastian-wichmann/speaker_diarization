b59963b36d0467be9820d73b6825c9ecdf05184f
Sebastian Wichmann <Sebastian.Wichmann1998@web.de>
2021-09-08 11:31:15 +0200

    Add logging to fbank extraction

--- src/pynn/bin/wav_to_fbank.py
+++ src/pynn/bin/wav_to_fbank.py
@@ -7,13 +7,16 @@
 import os
 import argparse
 import multiprocessing
+import threading
 import numpy as np
 from scipy.io import wavfile
 
 from pynn.util import audio
 from pynn.io import kaldi_io
 
-def write_ark_thread(segs, out_ark, out_scp, args):
+queue_end_string = "::end::"
+
+def write_ark_thread(segs, out_ark, out_scp, queue_default, queue_error, args):
     fbank_mat = audio.filter_bank(args.sample_rate, args.nfft, args.fbank)
     cache_wav = ''
     
@@ -30,18 +33,25 @@ def write_ark_thread(segs, out_ark, out_scp, args):
         
         if args.wav_path is not None:
             wav = wav if wav.endswith('.wav') else wav + '.wav'
-            wav = args.wav_path + '/' + wav
+            wav = os.path.join(args.wav_path, wav)
 
         if seg_name == '':
             seg_name = os.path.basename(wav)[:-4]
 
         if cache_wav != wav:
             if not os.path.isfile(wav):
-                print('File %s does not exist' % wav)
+                queue_default.put(f'File {wav} does not exist')
                 continue
-            sample_rate, signal = wavfile.read(wav)
+            try:
+                sample_rate, signal = wavfile.read(wav)
+            except ValueError as ex:
+                queue_error.put(f'Value error occurred while reading file {wav}\n'
+                                f'Error parameter: {ex.args}\n'
+                                f'Default message: {ex}')
+
+
             if sample_rate != args.sample_rate:
-                print('Wav %s is not in desired sample rate' % wav)
+                queue_default.put(f'Wav {wav} is not in desired sample rate')
                 continue
             cache_wav = wav
 
@@ -50,7 +60,7 @@ def write_ark_thread(segs, out_ark, out_scp, args):
             seg_name = '%s-%06.f-%06.f' % (seg_name, start*100, end*100)
         start, end = int(start * sample_rate), int(end * sample_rate)
         if start >= len(signal) or start >= end >= 0:
-            print('Wrong segment %s' % seg_name)
+            queue_default.put(f'Wrong segment {seg_name}')
             continue
 
         feats = audio.extract_fbank(signal[start:end], fbank_mat, sample_rate=sample_rate, nfft=args.nfft)
@@ -66,7 +76,22 @@ def write_ark_thread(segs, out_ark, out_scp, args):
         kaldi_io.write_ark_file(ark_file, scp_file, dic)
     ark_file.close()
     scp_file.close()
- 
+
+def write_queue_to_file(queue, file = None):
+    if file is not None:
+        with open(file, 'w') as out_handle:
+            while True:
+                value = queue.get()
+                if value == queue_end_string:
+                    break
+                out_handle.write(f'{value}\n')
+                print(value)
+    else:
+        while True:
+            value = queue.get()
+            if value == queue_end_string:
+                break
+            print(value)
 
 parser = argparse.ArgumentParser(description='pynn')
 parser.add_argument('--seg-desc', help='input segment description file', required=True)
@@ -81,10 +106,21 @@ parser.add_argument('--mean-norm', help='mean substraction', action='store_true'
 parser.add_argument('--fp16', help='use float16 instead of float32', action='store_true')
 parser.add_argument('--output', help='output file', type=str, default='data')
 parser.add_argument('--jobs', help='number of parallel jobs', type=int, default=1)
+parser.add_argument('--log-error', help='output file for error messages', type=str, default=None)
+parser.add_argument('--log-default', help='output file for error messages', type=str, default=None)
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    
+
+    manager = multiprocessing.Manager()
+    out_queue = manager.Queue()
+    error_queue = manager.Queue()
+
+    default_thread = threading.Thread(target=write_queue_to_file, args=(out_queue, args.log_default))
+    default_thread.start()
+    error_thread = threading.Thread(target=write_queue_to_file, args=(error_queue, args.log_error))
+    error_thread.start()
+
     segs = [line.rstrip('\n') for line in open(args.seg_desc, 'r')]
     size = len(segs) // args.jobs
     jobs = []
@@ -97,9 +133,14 @@ if __name__ == '__main__':
         out_scp = '%s.%d.scp' % (args.output, i)
          
         process = multiprocessing.Process(
-                target=write_ark_thread, args=(sub_segs, out_ark, out_scp, args))
+                target=write_ark_thread, args=(sub_segs, out_ark, out_scp, out_queue, error_queue, args))
         process.start()
         jobs.append(process)
     
     for job in jobs: job.join()
+
+    out_queue.put(queue_end_string)
+    error_queue.put(queue_end_string)
+    default_thread.join()
+    error_thread.join()
     
