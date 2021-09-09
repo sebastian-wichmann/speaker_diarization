ada7b0a8cdd165e52ae16b88b2e11ee24f298841
Sebastian Wichmann <Sebastian.Wichmann1998@web.de>
2021-09-09 15:53:18 +0200

    Match plda_trainer to match the data loader

--- src/pynn/trainer/plda.py
+++ src/pynn/trainer/plda.py
@@ -11,36 +11,32 @@ from torch.cuda.amp import autocast
 from . import EpochPool, ScheduledOptim
 from . import cal_ce_loss, load_last_chkpt, save_last_chkpt
 
-def train_model(model, datasets, epochs, device, cfg, fp16=False, dist=False):
+def train_model(model, dataset, device, cfg, fp16=False, dist=False):
     ''' Start training '''
     model_path = cfg['model_path']
 
-    data = datasets
-    data.initialize(b_input, b_sample)
+    #classes, data = datasets
+    dataset.initialize()
     
     start = time.time()
     model.train()
     
-    data_len = len(data)
-    loader = data.create_loader()
+    #data_len = len(dataset)
+    data = dataset.get()
 
-    for batch_i, batch in enumerate(loader):
-        # prepare data
-        src_seq, src_mask, tgt_seq = map(lambda x: x.to(device), batch)
-        last = (batch_i == data_len)
-        n_seq += tgt_seq.size(0)
+    # data = map(lambda x: x.to(device), data)
 
-        try:
-            # forward
-            with autocast(enabled=fp16):
-                model(src_seq, src_mask, tgt_seq, encoding=not sampling)[0]
-        except RuntimeError as err:
-            if 'CUDA out of memory' in str(err):
-                print('    WARNING: ran out of memory on GPU at %d' % n_seq)
-                torch.cuda.empty_cache(); continue
-            raise err
+    try:
+        # forward
+        with autocast(enabled=fp16):
+            model(data)
+    except RuntimeError as err:
+        if 'CUDA out of memory' in str(err):
+            print('    WARNING: ran out of memory on GPU')
+            torch.cuda.empty_cache()
+        raise err
 
-    print(f"  duration {(time.time()-start)/60):3.3f}")
+    print(f"  duration {(time.time()-start) / 60:3.3f}")
 
     model_file = path.join(model_path, 'plda.pt')
     torch.save(model.state_dict(), model_file)
